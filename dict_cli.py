345622c234c3ad845c201a3a1ba05583df9d9d53
Rigardt Engelbrecht <45070199+RWEngelbrecht@users.noreply.github.com>
2021-07-09 07:05:25 +0200

    checks for error message. prints better output

--- dict_cli.py
+++ dict_cli.py
@@ -5,19 +5,24 @@ app = typer.Typer()
 @app.command()
 def define(word:str):
   response = requests.get(f'https://api.dictionaryapi.dev/api/v2/entries/en_GB/{word}').json()
-  meanings = response[0]['meanings']
-  definitions = [{
-    'pos': meaning['partOfSpeech'],
-    'defs': [defi['definition'] for defi in meaning['definitions']]
-  } for meaning in meanings]
+  if "title" not in response:
+    # meanings = json.dumps(response[0]['meanings'], indent=2)
+    # print(json.dumps(response, indent=2))
+    meanings = response[0]['meanings']
+    definitions = [{
+      'pos': meaning['partOfSpeech'],
+      'defs': [defi['definition'] for defi in meaning['definitions']]
+    } for meaning in meanings]
 
-  for definition in definitions:
-    print(\
-    f'''\
-    PoS: {definition['pos']}
-    Def: {definition['defs']}
-    ''')
-  pass
+    for definition in definitions:
+      print(f'  PoS: {definition["pos"]}\n  Defs:')
+      for defi in definition['defs']:
+        print(f'\t{defi}')
+      print('')
+  else:
+    print('\tGuess this dictionary doesn\'t have all the answers...\n\
+        Or you did a typo.')
+
 
 @app.command()
 def synonym():
